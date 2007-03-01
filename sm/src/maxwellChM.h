8eeee0fef5f0eea3f51a11a2d1cfa597c5ab65a8
Bořek Patzák <borek.patzak@gmail.com>
2007-03-01 14:32:50 +0000

    Save&Restore context services migrated to use DataStream representation instead of plain FILE

--- sm/src/maxwellChM.h
+++ sm/src/maxwellChM.h
@@ -97,8 +97,8 @@ public:
  virtual void updateYourself(TimeStep*);    // update after new equilibrium state reached
 
  // saves current context(state) into stream
- contextIOResultType    saveContext (FILE* stream, void *obj = NULL);
- contextIOResultType    restoreContext(FILE* stream, void *obj = NULL);
+ contextIOResultType    saveContext (DataStream* stream, ContextMode mode, void *obj = NULL);
+ contextIOResultType    restoreContext(DataStream* stream, ContextMode mode, void *obj = NULL);
   
  // definition
  const char* giveClassName () const { return "MaxwellChainMaterialStatus" ;}
@@ -173,8 +173,8 @@ protected:
  void     printYourself () ;
 
  // store & restore context functions
- contextIOResultType    saveContext (FILE* stream, void *obj = NULL);
- contextIOResultType    restoreContext(FILE* stream, void *obj = NULL);
+ contextIOResultType    saveContext (DataStream* stream, ContextMode mode, void *obj = NULL);
+ contextIOResultType    restoreContext(DataStream* stream, ContextMode mode, void *obj = NULL);
 
 
  virtual void  give3dMaterialStiffnessMatrix (FloatMatrix& answer,
