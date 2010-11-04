094075b2112906987b9d97a677dd69742ffbfa05
Bořek Patzák <borek.patzak@gmail.com>
2010-11-04 10:44:05 +0000

    Cleanup of clock.h and clock.C files. Submitted by M. Ohman.

--- src/oofemlib/clock.C
+++ src/oofemlib/clock.C
@@ -43,18 +43,6 @@
 #include "clock.h"
 #include "compiler.h"
 #ifndef _MSC_VER
-#ifndef __MAKEDEPEND
-#if TIME_WITH_SYS_TIME
-#include <sys/time.h>
-#include <time.h>
-#else
-# if HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
-#endif
-#endif
 
 namespace oofem {
 
--- src/oofemlib/clock.h
+++ src/oofemlib/clock.h
@@ -42,14 +42,16 @@
 #ifndef clock_h
 #define clock_h
 
+#include "compiler.h"
+
 #ifndef _MSC_VER // If no Microsoft C
 
 #ifndef __MAKEDEPEND
-#if TIME_WITH_SYS_TIME
-#include <sys/time.h>
-#include <time.h>
+#ifdef TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
 #else
-# if HAVE_SYS_TIME_H
+# ifdef HAVE_SYS_TIME_H
 #  include <sys/time.h>
 # else
 #  include <time.h>
@@ -96,7 +98,6 @@ typedef timeval oofem_timeval;
  * Returns current time in seconds.
  * @return current time in oofem_timeval structure.
  */
-
 void getTime(oofem_timeval &answer);
 
 /**
