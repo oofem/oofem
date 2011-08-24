34e82a1d11999498b249396b8f5f716c19d773f1
Mikael Öhman <micketeer@gmail.com>
2011-08-24 11:28:29 +0000

    Missing files from previous commit. 3d quadratic tetrahedron.

---
+++ src/oofemlib/fei3dtetquad.C
@@ -0,0 +1,420 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2011   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "fei3dtetquad.h"
+#include "flotarry.h"
+#include "flotmtrx.h"
+#include "intarray.h"
+#include "node.h"
+#include "mathfem.h"
+
+namespace oofem {
+void
+FEI3dTetQuad :: evalN(FloatArray &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time)
+{
+    double x1 = lcoords(0);
+    double x2 = lcoords(1);
+    double x3 = lcoords(2);
+    double x4 = 1.0 - x1 - x2 - x3;
+
+    answer.resize(10);
+    answer(0) = x1*(2*x1-1);
+    answer(1) = x2*(2*x2-1);
+    answer(2) = x3*(2*x3-1);
+    answer(3) = x4*(2*x4-1);
+
+    answer(4) = 4*x1*x2;
+    answer(5) = 4*x2*x3;
+    answer(6) = 4*x3*x1;
+    answer(7) = 4*x1*x4;
+    answer(8) = 4*x2*x4;
+    answer(9) = 4*x3*x4;
+}
+
+void
+FEI3dTetQuad :: evaldNdx(FloatMatrix &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time)
+{
+    FloatMatrix jacobianMatrix;
+    FloatMatrix dNdxi;
+    FloatMatrix coords(10,3);
+    this->evaldNdxi(dNdxi, lcoords);
+    for (int i = 1; i <= 10; ++i) {
+        FloatArray *c = cellgeo.giveVertexCoordinates(i);
+        coords.at(i,1) = c->at(1);
+        coords.at(i,1) = c->at(2);
+        coords.at(i,1) = c->at(3);
+    }
+    jacobianMatrix.beProductOf(dNdxi, coords);
+    jacobianMatrix.solveForRhs(answer, dNdxi);
+}
+
+void
+FEI3dTetQuad :: evaldNdxi(FloatMatrix &answer, const FloatArray &lcoords)
+{
+    double x1 = lcoords(0);
+    double x2 = lcoords(1);
+    double x3 = lcoords(2);
+    double x4 = 1.0 - x1 - x2 - x3;
+
+    answer.resize(3,10);
+
+    // dNj/dx1
+    answer(0,0) = 4*x1 - 1;
+    answer(0,1) = 0;
+    answer(0,2) = 0;
+    answer(0,3) = -4*x4 + 1;
+    answer(0,4) = 4*x2;
+    answer(0,5) = 0;
+    answer(0,6) = 4*x3;
+    answer(0,7) = 4*(x4-x1);
+    answer(0,8) = -4*x2;
+    answer(0,9) = -4*x3;
+
+    // dNj/dx2
+    answer(1,0) = 0;
+    answer(1,1) = 4*x2 - 1;
+    answer(1,2) = 0;
+    answer(1,3) = -4*x4 + 1;
+    answer(1,4) = 4*x1;
+    answer(1,5) = 4*x3;
+    answer(1,6) = 0;
+    answer(1,7) = -4*x1;
+    answer(1,8) = 4*(x4-x2);
+    answer(1,9) = -4*x3;
+
+    // dNj/dx3
+    answer(2,0) = 0;
+    answer(2,1) = 0;
+    answer(2,2) = 4*x3 - 1;
+    answer(2,3) = -4*x4 + 1;
+    answer(2,4) = 0;
+    answer(2,5) = 4*x2;
+    answer(2,6) = 4*x1;
+    answer(2,7) = -4*x1;
+    answer(2,8) = -4*x2;
+    answer(2,9) = 4*(x4-x3);
+}
+
+
+void
+FEI3dTetQuad :: local2global(FloatArray &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time)
+{
+    FloatArray N;
+    this->evalN(N,lcoords, cellgeo, time);
+    answer.resize(0);
+    for ( int i = 1; i <= N.giveSize(); i++ ) {
+        answer.add( N(i), *cellgeo.giveVertexCoordinates(i));
+    }
+}
+
+int // TODO
+FEI3dTetQuad :: global2local(FloatArray &answer, const FloatArray &coords, const FEICellGeometry &cellgeo, double time)
+{
+    OOFEM_ERROR("FEI3dTetQuad :: global2local - Not supported");
+    return -1;
+}
+
+
+double
+FEI3dTetQuad :: giveTransformationJacobian(const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time)
+{
+    FloatMatrix jacobianMatrix;
+    this->giveJacobianMatrixAt(jacobianMatrix, lcoords, cellgeo);
+    return jacobianMatrix.giveDeterminant();
+}
+
+
+void
+FEI3dTetQuad :: giveJacobianMatrixAt(FloatMatrix &jacobianMatrix, const FloatArray &lcoords, const FEICellGeometry &cellgeo)
+{
+    FloatMatrix dNdxi;
+    FloatMatrix coords;
+    this->evaldNdxi(dNdxi, lcoords);
+    jacobianMatrix.resize(3,3);
+    coords.resize(10,3);
+    for (int i = 1; i <= 10; ++i) {
+        FloatArray *c = cellgeo.giveVertexCoordinates(i);
+        coords.at(i,1) = c->at(1);
+        coords.at(i,1) = c->at(2);
+        coords.at(i,1) = c->at(3);
+    }
+    jacobianMatrix.beProductOf(dNdxi, coords);
+}
+
+
+void
+FEI3dTetQuad :: edgeEvalN(FloatArray &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time)
+{
+    double ksi = lcoords.at(1);
+    answer.resize(3);
+    answer(0) = 0.5*(xi-1.0)*xi;
+    answer(1) = 0.5*(xi+1.0)*xi;
+    answer(2) = 1.0-xi*xi;
+}
+
+void // TODO
+FEI3dTetQuad :: edgeEvaldNdx(FloatMatrix &answer, int iedge,
+                           const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time)
+{
+    double coeff, l, x1, x2, y1, y2, z1, z2;
+    IntArray edgeNodes;
+    this->computeLocalEdgeMapping(edgeNodes, iedge);
+    OOFEM_ERROR("FEI3dTetQuad :: edgeEvaldNdx - Not supported");
+}
+
+void
+FEI3dTetQuad :: edgeLocal2global(FloatArray &answer, int iedge,
+                               const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time)
+{
+    IntArray edgeNodes;
+    FloatArray N;
+    this->computeLocalEdgeMapping(edgeNodes, iedge);
+    this->edgeEvalN(N, lcoords, cellgeo, time);
+
+    answer.resize(0);
+    for (int i = 0; i < N.giveSize(); ++i) {
+        answer.add( N(i), *cellgeo.giveVertexCoordinates( edgeNodes(i)) );
+    }
+}
+
+
+double // TODO
+FEI3dTetQuad :: edgeGiveTransformationJacobian(int iedge, const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time)
+{
+    IntArray edgeNodes;
+    this->computeLocalEdgeMapping(edgeNodes, iedge);
+    OOFEM_ERROR("FEI3dTetQuad :: edgeGiveTransformationJacobian - Not supported");
+    return -1;
+}
+
+
+void
+FEI3dTetQuad :: computeLocalEdgeMapping(IntArray &edgeNodes, int iedge)
+{
+    int aNode = 0, bNode = 0, cNode = 0;
+    edgeNodes.resize(3);
+
+    if ( iedge == 1 ) { // edge between nodes 1 2
+        edgeNodes(0) = 1;
+        edgeNodes(1) = 2;
+        edgeNodes(2) = 5;
+    } else if ( iedge == 2 ) { // edge between nodes 2 3
+        edgeNodes(0) = 2;
+        edgeNodes(1) = 3;
+        edgeNodes(2) = 6;
+    } else if ( iedge == 3 ) { // edge between nodes 3 1
+        edgeNodes(0) = 3;
+        edgeNodes(1) = 1;
+        edgeNodes(2) = 7;
+    } else if ( iedge == 4 ) { // edge between nodes 1 4
+        edgeNodes(0) = 1;
+        edgeNodes(1) = 4;
+        edgeNodes(2) = 8;
+    } else if ( iedge == 5 ) { // edge between nodes 2 4
+        edgeNodes(0) = 2;
+        edgeNodes(1) = 4;
+        edgeNodes(2) = 9;
+    } else if ( iedge == 6 ) { // edge between nodes 3 4
+        edgeNodes(0) = 3;
+        edgeNodes(1) = 4;
+        edgeNodes(2) = 10;
+    } else {
+        OOFEM_ERROR2("FEI3dTetQuad :: computeEdgeMapping: wrong egde number (%d)", iedge);
+    }
+}
+
+double // TODO
+FEI3dTetQuad :: edgeComputeLength(IntArray &edgeNodes, const FEICellGeometry &cellgeo)
+{
+    OOFEM_ERROR("FEI3dTetQuad :: edgeComputeLength - Not supported");
+    return -1;
+}
+
+void
+FEI3dTetQuad :: surfaceEvalN(FloatArray &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time)
+{
+    double l1 = lcoords.at(1);
+    double l2 = lcoords.at(2);
+    double l3 = 1. - l1 - l2;
+
+    answer.resize(6);
+
+    answer.at(1) = ( 2. * l1 - 1. ) * l1;
+    answer.at(2) = ( 2. * l2 - 1. ) * l2;
+    answer.at(3) = ( 2. * l3 - 1. ) * l3;
+    answer.at(4) = 4. * l1 * l2;
+    answer.at(5) = 4. * l2 * l3;
+    answer.at(6) = 4. * l3 * l1;
+}
+
+void
+FEI3dTetQuad :: surfaceLocal2global(FloatArray &answer, int iedge,
+                                  const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time)
+{
+    IntArray nodes(3);
+    FloatArray N(3);
+    this->computeLocalSurfaceMapping(nodes, iedge);
+    this->surfaceEvalN(N, lcoords, cellgeo, 0,0);
+
+    answer.resize(0);
+    for (int i = 0; i < N.giveSize(); ++i) {
+        answer.add( n(i), *cellgeo.giveVertexCoordinates(nodes(i)) );
+    }
+}
+
+void
+FEI3dTetQuad :: surfaceEvaldNdx(FloatMatrix &answer, int isurf, const FloatArray &lcoords, const FEICellGeometry &cellgeo)
+{
+    // Translate the local surface coordinate to the volume coordinates and compute the gradient there.
+    double a, b, c;
+    a = lcoords.at(1);
+    b = lcoords.at(2);
+    c = 1.0 - a - b;
+    FloatArray lcoords_tet(4);
+    lcoords_tet.at(isurf) = 0.0;
+    if (isurf == 1) {
+        lcoords_tet.at(4) = a;
+        lcoords_tet.at(3) = b;
+        lcoords_tet.at(2) = c;
+    } else if (isurf == 2) {
+        lcoords_tet.at(1) = a;
+        lcoords_tet.at(3) = b;
+        lcoords_tet.at(4) = c;
+    } else if (isurf == 3) {
+        lcoords_tet.at(1) = a;
+        lcoords_tet.at(4) = b;
+        lcoords_tet.at(2) = c;
+    } else if (isurf == 4) {
+        lcoords_tet.at(2) = a;
+        lcoords_tet.at(3) = b;
+        lcoords_tet.at(1) = c;
+    }
+    this->evaldNdx(answer, lcoords_tet, cellgeo, time);
+}
+
+void
+FEI3dTetQuad :: surfaceEvalNormal(FloatArray &answer, int isurf, const FloatArray &lcoords, const FEICellGeometry &cellgeo)
+{
+    double dNdxi;
+    double dNdeta;
+    IntArray snodes(3);
+    FloatArray a,b;
+    this->computeLocalSurfaceMapping(snodes, isurf);
+
+    double l1, l2, l3;
+    l1 = lcoords.at(1);
+    l2 = lcoords.at(2);
+    l3 = 1.0 - l1 - l2;
+
+    FloatArray dNdxi(6), dNdeta(6);
+
+    dNdxi(0) = 4.0 * l1 - 1.0;
+    dNdxi(1) = 0.0;
+    dNdxi(2) = -1.0 * ( 4.0 * l3 - 1.0 );
+    dNdxi(3) = 4.0 * l2;
+    dNdxi(4) = -4.0 * l2;
+    dNdxi(5) = 4.0 * l3 - 4.0 * l1;
+
+    dNdeta(0) = 0.0;
+    dNdeta(1) = 4.0 * l2 - 1.0;
+    dNdeta(2) = -1.0 * ( 4.0 * l3 - 1.0 );
+    dNdeta(3) = 4.0 * l1;
+    dNdeta(4) = 4.0 * l3 - 4.0 * l2;
+    dNdeta(5) = -4.0 * l1;
+
+    for (int i = 0; i < 6; ++i) {
+        a.add(dNdxi(i),  cellgeo.giveVertexCoordinates(snodes(i)));
+        b.add(dNdeta(i), cellgeo.giveVertexCoordinates(snodes(i)));
+    }
+    answer.beVectorProductOf(a, b);
+    double J = answer.computeNorm();
+    answer.times(1/J);
+    return J;
+}
+
+double
+FEI3dTetQuad :: surfaceGiveTransformationJacobian(int isurf, const FloatArray &lcoords,
+                                                const FEICellGeometry &cellgeo, double time)
+{
+    FloatArray normal;
+    return this->surfaceEvalNormal(normal, isurf, lcoords, cellgeo);
+}
+
+void
+FEI3dTetQuad :: computeLocalSurfaceMapping(IntArray &surfNodes, int isurf)
+{
+    int aNode = 0, bNode = 0, cNode = 0;
+    surfNodes.resize(3);
+
+    if ( isurf == 1 ) {
+        aNode = 1;
+        bNode = 3;
+        cNode = 2;
+        dNode = 7;
+        eNode = 6;
+        fNode = 5;
+    } else if ( isurf == 2 ) {
+        aNode = 1;
+        bNode = 2;
+        cNode = 4;
+        dNode = 5;
+        eNode = 9;
+        fNode = 8;
+    } else if ( isurf == 3 ) {
+        aNode = 2;
+        bNode = 3;
+        cNode = 4;
+        dNode = 6;
+        eNode = 10;
+        fNode = 9;
+    } else if ( isurf == 4 ) {
+        aNode = 1;
+        bNode = 4;
+        cNode = 3;
+        dNode = 8;
+        eNode = 10;
+        fNode = 7;
+    } else {
+        OOFEM_ERROR2("FEI3dTetQuad :: computeLocalSurfaceMapping: wrong surface number (%d)", isurf);
+    }
+
+    surfNodes.at(1) = aNode;
+    surfNodes.at(2) = bNode;
+    surfNodes.at(3) = cNode;
+    surfNodes.at(4) = aNode;
+    surfNodes.at(5) = bNode;
+    surfNodes.at(6) = cNode;
+
+}
+} // end namespace oofem
---
+++ src/oofemlib/fei3dtetquad.h
@@ -0,0 +1,93 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2011   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef fei3dtrlin_h
+#define fei3dtrlin_h
+
+#include "feinterpol3d.h"
+
+namespace oofem {
+/**
+ * Class representing implementation of quadratic tetrahedra interpolation class.
+ * @author Mikael Öhman
+ * @todo This class is entirely unchecked.
+ */
+class FEI3dTetQuad : public FEInterpolation3d
+{
+public:
+    FEI3dTetQuad() : FEInterpolation3d(1) { }
+
+    // Bulk
+    virtual void evalN(FloatArray &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time);
+    virtual void evaldNdx(FloatMatrix &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time);
+    virtual void local2global(FloatArray &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time);
+    virtual int  global2local(FloatArray &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time);
+    virtual double giveTransformationJacobian(const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time);
+
+    // Edge
+    virtual void edgeEvalN(FloatArray &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time);
+    virtual void edgeEvaldNdx(FloatMatrix &answer, int iedge,
+                              const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time);
+    virtual void edgeLocal2global(FloatArray &answer, int iedge,
+                                  const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time);
+    virtual double edgeGiveTransformationJacobian(int iedge, const FloatArray &lcoords,
+                                                  const FEICellGeometry &cellgeo, double time);
+    virtual void computeLocalEdgeMapping(IntArray &edgeNodes, int iedge);
+
+    // Surface
+    virtual void surfaceEvalN(FloatArray &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time);
+    virtual void surfaceEvaldNdx(FloatMatrix &answer, int isurf,
+                   const FloatArray &lcoords, const FEICellGeometry &cellgeo);
+    virtual void surfaceEvalNormal(FloatArray &answer, int isurf,
+                   const FloatArray &lcoords, const FEICellGeometry &cellgeo);
+    virtual void surfaceLocal2global(FloatArray &answer, int isurf,
+                                     const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time);
+    virtual double surfaceGiveTransformationJacobian(int isurf, const FloatArray &lcoords,
+                                                     const FEICellGeometry &cellgeo, double time);
+    virtual void computeLocalSurfaceMapping(IntArray &edgeNodes, int iedge);
+
+protected:
+    double edgeComputeLength(IntArray &edgeNodes, const FEICellGeometry &cellgeo);
+
+    virtual void giveJacobianMatrixAt(FloatMatrix &jacobianMatrix, const FloatArray &lcoords, const FEICellGeometry &cellgeo);
+    void evaldNdxi(FloatMatrix &answer, const FloatArray &lcoords);
+};
+} // end namespace oofem
+#endif // fei3dtrlin_h
+
+
+
+
+
+
