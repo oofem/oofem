63b40d9e8a0692d532d11f421187fb71aff6e6d0
Bořek Patzák <borek.patzak@gmail.com>
2011-09-04 17:24:10 +0000

    Adding representation of field defined by nodal values.

---
+++ src/oofemlib/dofmanvalfield.C
@@ -0,0 +1,117 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2011   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "dofmanvalfield.h"
+#include "spatiallocalizer.h"
+#include "usrdefsub.h"
+
+namespace oofem {
+DofManValueField :: DofManValueField(FieldType ft, Domain *d) : Field(ft), dmanvallist()
+{
+    int i, ndofman = d->giveNumberOfDofManagers();
+    this->domain = d;
+    this->dmanvallist.growTo(ndofman);
+    for (i=1;i<=ndofman; i++) {
+      this->dmanvallist.put(i, new FloatArray);
+    }
+}
+
+int
+DofManValueField :: evaluateAt(FloatArray &answer, FloatArray &coords, ValueModeType mode, TimeStep *atTime)
+{
+  int i, result = 0; // assume ok
+  FloatArray lc, n;
+
+  // request element containing target point
+  Element* elem = this->domain->giveSpatialLocalizer()->giveElementContainingPoint(coords);
+  if (elem) { // ok element containing target point found
+    FEInterpolation* interp = elem->giveInterpolation();
+    if (interp) {
+      // map target point to element local coordinates
+      if (interp->global2local(lc, coords, FEIElementGeometryWrapper(elem), atTime->giveTargetTime())) {
+	// evaluate interpolation functions at target point
+	interp->evalN(n, lc, FEIElementGeometryWrapper(elem), atTime->giveTargetTime());
+	// loop over element nodes
+	for (i=1; i<=n.giveSize(); i++) {
+	  // multiply nodal value by value of corresponding shape function and add this to answer
+	  answer.add(n.at(i), *this->dmanvallist.at(elem->giveDofManagerNumber(i)));
+	}
+      } else { // mapping from global to local coordinates failed
+	result = 1; // failed
+      }
+    } else {  // element without interpolation 
+      result = 1; // failed
+    }
+  } else { // no element containing given point found
+    result = 1; // failed
+  }
+  return result; 
+}
+  
+int
+DofManValueField::evaluateAt(FloatArray &answer, DofManager* dman, ValueModeType mode, TimeStep *atTime)
+{
+  answer=*this->dmanvallist.at(dman->giveNumber());
+  return 1;
+}
+
+void
+DofManValueField::setDofManValue (int dofMan, const FloatArray &value)
+{
+  (*this->dmanvallist.at(dofMan)) = value;
+}
+
+contextIOResultType
+DofManValueField :: saveContext(DataStream *stream, ContextMode mode)
+{
+    // int i, type_id = SmoothedNodalInternalVariableFieldClass;
+    // contextIOResultType iores;
+    // write class header
+    // if (!stream->write(&type_id,1)) return CIO_IOERR;
+
+    return CIO_OK;
+}
+
+contextIOResultType
+DofManValueField :: restoreContext(DataStream *stream, ContextMode mode)
+{
+    // int i, class_id;
+    // contextIOResultType iores;
+    // read class header
+    // if (!stream->read(&class_id,1)) return CIO_IOERR;
+    // if (class_id != SmoothedNodalInternalVariableField) return CIO_BADVERSION;
+
+    return CIO_OK;
+}
+} // end namespace oofem
---
+++ src/oofemlib/dofmanvalfield.h
@@ -0,0 +1,122 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2011   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef dofmanvalfield_h
+#define dofmanvalfield_h
+
+#include "domain.h"
+#include "field.h"
+
+namespace oofem {
+
+/**
+ * Class representing field defined by nodal values assiciated to given domain. 
+ * Field represent the spatial distribution of certain variable.
+ * The implementation allows to set individual dofMan values;
+ * However, in the current implementation doe not allow to specify values for different time steps.
+ */
+  class DofManValueField : public Field
+{
+protected:
+  /// Associated domain (need its elements to interpolate)
+  Domain* domain;
+  /// Array of dofman values
+  AList <FloatArray> dmanvallist;
+
+
+
+public:
+    /** 
+     * Constructor. Creates an empty field of given type associated to given domain.
+     */
+    DofManValueField(FieldType b, Domain *d);
+    ~DofManValueField() {}
+    /**
+     * Evaluates the field at given point.
+     * @param coords Coordinates of the point of interest
+     * @param answer Field evaluated at coordinate.
+     * @param atTime Time step to evaluate for.
+     * @param mode Mode of value (total, velocity,...).
+     * @return Zero if ok, otherwise nonzero.
+     */
+    virtual int evaluateAt(FloatArray &answer, FloatArray &coords,
+                           ValueModeType mode, TimeStep *atTime);
+
+    /**
+     * Evaluates the field at given DofManager. This potentially can be resolved quickly, as
+     * receiver data may be described using values at dofManagers. Here an additional issue
+     * exists: one needs to make sure, that passed dman is from the same domain, so that its
+     * number can be used to perform suggested quick evaluation.
+     *
+     * If this is not the case (the field is described differently),
+     * the response can be evaluated using dofman coordinates in a standard way.
+     * @param[out] answer Evaluated field for dman.
+     * @param dman Reference to dofManager.
+     * @param mode Mode of value (total, velocity,...).
+     * @param atTime Time step to evaluate for.
+     * @return Zero if ok, nonzero Error code (0-ok, 1-failed)
+     */
+    virtual int evaluateAt(FloatArray &answer, DofManager* dman,
+                           ValueModeType mode, TimeStep *atTime) ;
+    
+    /**
+     * Stores receiver state to output stream.
+     * Writes the FEMComponent class-id in order to allow test whether correct data are then restored.
+     * @param stream Output stream.
+     * @param mode Determines amount of info in stream (state, definition,...).
+     * @return contextIOResultType.
+     * @exception Throws an ContextIOERR exception if error encountered.
+     */
+    virtual contextIOResultType saveContext(DataStream *stream, ContextMode mode) ;
+    /**
+     * Restores the receiver state previously written in stream.
+     * Reads the FEMComponent class-id in order to allow test consistency.
+     * @param stream Input stream.
+     * @param mode Determines amount of info in stream (state, definition,...).
+     * @return contextIOResultType.
+     * @exception Throws an ContextIOERR exception if error encountered.
+     */
+    virtual contextIOResultType restoreContext(DataStream *stream, ContextMode mode) ;
+
+
+    /**
+     * Sets the value associated to given dofManager
+     */
+    void setDofManValue (int dofMan, const FloatArray &value); 
+
+    /// @return Class name of the receiver.
+    virtual const char *giveClassName() const { return "DofManValueField"; }
+};
+} // end namespace oofem
+#endif // dofmanvalfield_h
