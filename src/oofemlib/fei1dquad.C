3722054ee59d8b5a77cd6dadc657a5ef0c26455d
Mikael Öhman <micketeer@gmail.com>
2011-10-02 23:17:07 +0000

    Fixed abs() to fabs() for some doubles.

--- src/oofemlib/fei1dquad.C
+++ src/oofemlib/fei1dquad.C
@@ -56,14 +56,14 @@ FEI1dQuad :: evalN(FloatArray &answer, const FloatArray &lcoords, const FEICellG
 void
 FEI1dQuad :: evaldNdx(FloatMatrix &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time)
 {
-  double J = this->giveTransformationJacobian(lcoords,cellgeo,time);
-  double ksi = lcoords.at(1);
-  answer.resize(1, 3);
-  answer.zero();
-  
-  answer.at(1, 1) = (-1./2. + ksi)/J;
-  answer.at(1, 2) =  (1./2. + ksi)/J;
-  answer.at(1, 3) =  -2.*ksi/J;
+    double J = this->giveTransformationJacobian(lcoords,cellgeo,time);
+    double ksi = lcoords.at(1);
+    answer.resize(1, 3);
+    answer.zero();
+
+    answer.at(1, 1) = (-1./2. + ksi)/J;
+    answer.at(1, 2) =  (1./2. + ksi)/J;
+    answer.at(1, 3) =  -2.*ksi/J;
 }
 
 void
@@ -74,73 +74,73 @@ FEI1dQuad :: local2global(FloatArray &answer, const FloatArray &lcoords, const F
 
     this->evalN(n, lcoords, cellgeo, time);
     answer.at(1) = ( n.at(1) * cellgeo.giveVertexCoordinates(1)->at(cindx) +
-                    n.at(2) * cellgeo.giveVertexCoordinates(2)->at(cindx) + n.at(3) * cellgeo.giveVertexCoordinates(3)->at(cindx) );
+            n.at(2) * cellgeo.giveVertexCoordinates(2)->at(cindx) + n.at(3) * cellgeo.giveVertexCoordinates(3)->at(cindx) );
 }
 
 int
 FEI1dQuad :: global2local(FloatArray &answer, const FloatArray &coords, const FEICellGeometry &cellgeo, double time)
 {
-  double  x1, x2, x3;
-  answer.resize(1);
-  double a,b,c;
-  
+    double  x1, x2, x3;
+    answer.resize(1);
+    double a,b,c;
+
     x1 = cellgeo.giveVertexCoordinates(1)->at(cindx);
     x2 = cellgeo.giveVertexCoordinates(2)->at(cindx);
     x3 = cellgeo.giveVertexCoordinates(3)->at(cindx);
-    
+
     a = 1./2.*(x1+x2)-x3;
     b = 1./2.*(x2-x1);
     c = x3 - coords.at(1);
-    
-    if(abs(a)<10.e-6)
-      {
-	double ksi;
-	 answer.at(1) = ksi = ( 2.0 * coords.at(1) - ( x1 + x2 ) ) / ( x2 - x1 );
-	 return ( fabs(ksi) <= 1.0 ) ? 1 : 0;
-      }
+
+    if(fabs(a)<10.e-6)
+    {
+        double ksi;
+        answer.at(1) = ksi = ( 2.0 * coords.at(1) - ( x1 + x2 ) ) / ( x2 - x1 );
+        return ( fabs(ksi) <= 1.0 ) ? 1 : 0;
+    }
     else
-      {
-	double ksi1 = (-b+sqrt(b*b-4*a*c))/(2.*a);
-	double ksi2 = (-b-sqrt(b*b-4*a*c))/(2.*a);
-	
-	if((abs(ksi1)<=1)&&(abs(ksi2)<=1))
-	  {
-	    // _error("FEI1dQuad::global2local:Element is not well defined");
-	    return 0;
-	  }
-	
-	else if (abs(ksi1)<=1)
-	  {
-	    answer.at(1) = ksi1;
-	    return 1;
-	  }
-	else if(abs(ksi2)<=1)
-	  {
-	    answer.at(1) = ksi2;
-	    return 1;
-	  }
-	else
-	  {
-	    return 0;
-	  }
-      }
+    {
+        double ksi1 = (-b+sqrt(b*b-4*a*c))/(2.*a);
+        double ksi2 = (-b-sqrt(b*b-4*a*c))/(2.*a);
+
+        if((fabs(ksi1)<=1)&&(fabs(ksi2)<=1))
+        {
+            // _error("FEI1dQuad::global2local:Element is not well defined");
+            return 0;
+        }
+
+        else if (fabs(ksi1)<=1)
+        {
+            answer.at(1) = ksi1;
+            return 1;
+        }
+        else if(fabs(ksi2)<=1)
+        {
+            answer.at(1) = ksi2;
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 }
 
 double
 FEI1dQuad :: giveTransformationJacobian(const FloatArray &lcoords, const FEICellGeometry &cellgeo, double time)
 {
-  
-  double x1, x2, x3, J, ksi;
 
-  
-  x1 = cellgeo.giveVertexCoordinates(1)->at(cindx);
-  x2 = cellgeo.giveVertexCoordinates(2)->at(cindx);
-  x3 = cellgeo.giveVertexCoordinates(3)->at(cindx);
-  ksi = lcoords.at(1);
-  
-  J = 1./2.*(2*ksi-1)*x1 + 1./2.*(2*ksi+1)*x2 - 2.*ksi*x3; 
-  return J;
- 
+    double x1, x2, x3, J, ksi;
+
+
+    x1 = cellgeo.giveVertexCoordinates(1)->at(cindx);
+    x2 = cellgeo.giveVertexCoordinates(2)->at(cindx);
+    x3 = cellgeo.giveVertexCoordinates(3)->at(cindx);
+    ksi = lcoords.at(1);
+
+    J = 1./2.*(2*ksi-1)*x1 + 1./2.*(2*ksi+1)*x2 - 2.*ksi*x3;
+    return J;
+
 }
 
 double
