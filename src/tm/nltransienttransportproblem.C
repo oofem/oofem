ef97851e4d4d537cc62c9b37383684cc1c458dfc
Bořek Patzák <borek.patzak@gmail.com>
2012-03-12 10:16:17 +0000

    Fixed memory leak due to multiple dealocation of stepWhenIcApply in nltransientproblem.C.

--- src/tm/nltransienttransportproblem.C
+++ src/tm/nltransienttransportproblem.C
@@ -123,7 +123,7 @@ void NLTransientTransportProblem :: solveYourselfAt(TimeStep *tStep) {
     //create previous solution from IC or from previous tStep
     if ( tStep->isTheFirstStep() ) {
         if(stepWhenIcApply==NULL){
-            stepWhenIcApply = tStep->givePreviousStep();
+	  stepWhenIcApply = new TimeStep(*tStep->givePreviousStep());
         }
         this->applyIC(stepWhenIcApply); //insert solution to hash=1(previous), if changes in equation numbering
     }
