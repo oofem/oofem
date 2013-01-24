25e95630ff066f0a5a28943aeeb24402d729fc33
Bořek Patzák <borek.patzak@gmail.com>
2013-01-24 10:00:32 +0100

    Fixing the bug in IncrementalLinearStatic :: updateDofUnknownsDictionary. Linked (slave) DOFs are now skipped, only primary DOFs updated

--- src/sm/incrementallinearstatic.C
+++ src/sm/incrementallinearstatic.C
@@ -293,6 +293,8 @@ void IncrementalLinearStatic :: updateDofUnknownsDictionary(DofManager *inode, T
     double val;
     for ( int i = 1; i <= ndofs; i++ ) {
         iDof = inode->giveDof(i);
+	// skip slave DOFs (only master (primary) DOFs have to be updated).
+	if (!iDof->isPrimaryDof()) continue;
         val = iDof->giveUnknown(EID_MomentumBalance, VM_Total, tStep);
         if ( !iDof->hasBc(tStep) ) {
             val += this->incrementOfDisplacementVector.at( iDof->__giveEquationNumber() );
