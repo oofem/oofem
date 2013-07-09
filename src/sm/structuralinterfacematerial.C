2a0011586f00b3fb352da3c6b781346a4db4a7c3
Jim Brouzoulis <jim.brouzoulis@chalmers.se>
2013-07-09 09:12:46 +0200

    Added shell for new structural interface material class in order to have a distinct separation between a structural material and a structural interface

---
+++ src/sm/structuralinterfacematerial.C
@@ -0,0 +1,93 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2013   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "StructuralInterfaceMaterial.h"
+#include "structuralcrosssection.h"
+#include "domain.h"
+#include "verbose.h"
+#include "structuralms.h"
+#include "structuralelement.h"
+#include "nlstructuralelement.h"
+#include "gausspoint.h"
+#include "floatmatrix.h"
+#include "floatarray.h"
+#include "mathfem.h"
+#include "engngm.h"
+#include "fieldmanager.h"
+#include "dynamicinputrecord.h"
+
+namespace oofem {
+int
+StructuralInterfaceMaterial :: hasMaterialModeCapability(MaterialMode mode)
+//  
+// returns whether receiver supports given mode
+//
+{
+    return mode == _1dMat  ||  mode == _2dMat ||
+           mode == _3dMat;
+}
+
+
+void
+StructuralInterfaceMaterial :: giveFirstPKTraction(FloatArray &answer, GaussPoint *gp, const FloatArray &jump,
+                                         const FloatArray &reducedF, TimeStep *tStep)
+{
+    // compute F
+    // compute jump vector
+    // transform to local orthogonal coord system
+    // call stress from material
+}
+
+
+
+IRResultType
+StructuralInterfaceMaterial :: initializeFrom(InputRecord *ir)
+{
+    const char *__proc = "initializeFrom"; // Required by IR_GIVE_FIELD macro
+    IRResultType result;                // Required by IR_GIVE_FIELD macro
+
+    this->Material :: initializeFrom(ir);
+    return IRRT_OK;
+}
+
+
+void
+StructuralInterfaceMaterial :: giveInputRecord(DynamicInputRecord &input)
+{
+    Material :: giveInputRecord(input);
+    //input.setField(this->referenceTemperature, _IFT_StructuralInterfaceMaterial_referencetemperature);
+}
+
+
+} // end namespace oofem
