434d36dadaafbc88a2831014c3c5b24f51a17d84
Mikael Öhman <micketeer@gmail.com>
2013-12-05 14:05:07 +0100

    Added crosssection for transport module.

---
+++ src/tm/simpletransportcrosssection.C
@@ -0,0 +1,104 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2013   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "simpletransportcrosssection.h"
+#include "transportmaterial.h"
+#include "dynamicinputrecord.h"
+#include "classfactory.h"
+
+namespace oofem {
+
+REGISTER_CrossSection( SimpleTransportCrossSection );
+
+SimpleTransportCrossSection :: SimpleTransportCrossSection(int n, Domain *d) : TransportCrossSection(n, d) { }
+
+SimpleTransportCrossSection :: ~SimpleTransportCrossSection() { }
+
+
+IRResultType
+SimpleTransportCrossSection :: initializeFrom(InputRecord *ir)
+{
+    const char *__proc = "initializeFrom"; // Required by IR_GIVE_FIELD macro
+    IRResultType result;                // Required by IR_GIVE_FIELD macro
+
+    TransportCrossSection :: initializeFrom(ir);
+
+    IR_GIVE_FIELD(ir, this->matNumber, _IFT_SimpleTransportCrossSection_material);
+
+    return IRRT_OK;
+}
+
+
+void
+SimpleTransportCrossSection :: giveInputRecord(DynamicInputRecord &input)
+{
+    TransportCrossSection :: giveInputRecord(input);
+
+    input.setField(this->matNumber, _IFT_SimpleTransportCrossSection_material);
+}
+
+
+int
+SimpleTransportCrossSection :: checkConsistency()
+{
+    Material *mat = dynamic_cast< TransportMaterial* >( this->domain->giveMaterial(this->matNumber) );
+    if ( !mat ) {
+        return 0;
+    }
+    return TransportCrossSection :: checkConsistency();
+}
+
+
+TransportMaterial *
+SimpleTransportCrossSection :: giveMaterial()
+{
+    return dynamic_cast< TransportMaterial* >( this->domain->giveMaterial(this->matNumber) );
+}
+
+
+int
+SimpleTransportCrossSection :: giveIPValue(FloatArray &answer, GaussPoint *ip, InternalStateType type, TimeStep *atTime)
+{
+    return this->domain->giveMaterial(this->matNumber)->giveIPValue(answer, ip, type, atTime);
+}
+
+
+bool
+SimpleTransportCrossSection :: isCharacteristicMtrxSymmetric(MatResponseMode rMode)
+{
+    return this->domain->giveMaterial(this->matNumber)->isCharacteristicMtrxSymmetric(rMode);
+}
+
+
+} // end namespace oofem
---
+++ src/tm/transportcrosssection.h
@@ -0,0 +1,72 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2013   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef transportcrosssection_h
+#define transportcrosssection_h
+
+#include "crosssection.h"
+#include "gausspoint.h"
+#include "floatarray.h"
+#include "floatmatrix.h"
+
+#define _IFT_SimpleTransportCrossSection_material "mat"
+
+namespace oofem {
+
+class TransportMaterial;
+
+/**
+ * Transort cross-section. It's functionality is to be a wrapper around the material behavior.
+ * 
+ * @author Mikael Öhman
+ */
+class OOFEM_EXPORT TransportCrossSection : public CrossSection
+{
+public:
+    /**
+     * Constructor. Creates cross section with number n belonging to domain d.
+     * @param n Cross section number.
+     * @param d Domain for cross section.
+     */
+    TransportCrossSection(int n, Domain *d) : CrossSection(n, d) { }
+
+    /// Destructor.
+    virtual ~TransportCrossSection() {};
+
+    /// @todo Temporary function that hands out the material. Must be removed for future layered support, but input files will still look the same.
+    TransportMaterial *giveMaterial();
+};
+} // end namespace oofem
+#endif // transportcrosssection_h
+
