591277a1e28207a94cca1f3deca5eaa8e2aa4b4c
Erik Svenning <erik.svenning@chalmers.se>
2014-02-12 15:01:10 +0100

    sparsenonlinsystemnm.h: Added forward declaration of SparseLinearSystemNM.

--- src/oofemlib/sparsenonlinsystemnm.h
+++ src/oofemlib/sparsenonlinsystemnm.h
@@ -50,6 +50,7 @@ class EngngModel;
 class SparseMtrx;
 class FloatArray;
 class TimeStep;
+class SparseLinearSystemNM;
 
 /**
  * This base class is an abstraction for all numerical methods solving sparse
