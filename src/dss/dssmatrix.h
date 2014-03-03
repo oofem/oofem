c9704ca15beb8106f19d4afd14e3d15e0a7671a9
Bořek Patzák <borek.patzak@gmail.com>
2014-03-03 10:25:22 +0100

    Fixing DSS module compilation errror: Adding missing giveClassName to DSSMatrix

--- src/dss/dssmatrix.h
+++ src/dss/dssmatrix.h
@@ -103,6 +103,8 @@ public:
     double at(int i, int j) const;
     SparseMtrxType giveType() const { return SMT_SymCompCol; }
     bool isAsymmetric() const { return false; }
+
+    virtual const char *giveClassName() const { return "DSSMatrix";;}
 };
 
 class DSSMatrixLDL : public DSSMatrix
