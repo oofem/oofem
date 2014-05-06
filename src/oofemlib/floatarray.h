38a5e683480918c81d42794ff1432739a7f5fe93
Erik Svenning <erik.svenning@chalmers.se>
2014-05-06 16:17:42 +0200

    FloatArray: implemented append and push_back functions.

--- src/oofemlib/floatarray.h
+++ src/oofemlib/floatarray.h
@@ -76,6 +76,7 @@ class CommunicationBuffer;
  *   initialize.
  * 
  * @author Mikael Öhman
+ * @author Erik Svenning
  * @author many others (please add yourselves)
  */
 class OOFEM_EXPORT FloatArray
@@ -112,6 +113,12 @@ public:
     FloatArray &operator = (FloatArray &&src) { values = std::move(src.values); return *this; }
     /// Assignment operator.
     inline FloatArray &operator = (std :: initializer_list< double >list) { values = list; return *this; }
+
+    /// Add one element
+    void push_back(const double &iVal) {values.push_back(iVal);}
+
+    /// Append an array
+    void append(const FloatArray &iArray) { values.insert(values.end(), iArray.begin(), iArray.end()); }
 
     /**
      * Coefficient access function. Returns value of coefficient at given
