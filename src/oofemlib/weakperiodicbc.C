45c1c2445d798d8b2dd8443e4aed1886aff4bdde
Carl Sandström <carl.sandstrom@chalmers.se>
2014-10-07 20:45:35 +0200

    Fixed error in weakpreiodicbc

--- src/oofemlib/weakperiodicbc.C
+++ src/oofemlib/weakperiodicbc.C
@@ -746,7 +746,6 @@ WeakPeriodicBoundaryCondition :: giveExternalForcesVector(FloatArray &answer, Ti
 
                 // Find the value of parameter s which is the vert/horiz distance to 0
                 geoInterpolation->boundaryLocal2Global( gcoords, side [ thisSide ].at(ielement), * lcoords, FEIElementGeometryWrapper( thisElement ) );
-                double s = gcoords.at( surfaceIndexes.at(1) );
                 // Compute Jacobian
                 double detJ = fabs( geoInterpolation->boundaryGiveTransformationJacobian( side [ thisSide ].at(ielement), * lcoords, FEIElementGeometryWrapper(thisElement) ) );
 
@@ -757,14 +756,14 @@ WeakPeriodicBoundaryCondition :: giveExternalForcesVector(FloatArray &answer, Ti
 
                     if ( this->domain->giveNumberOfSpatialDimensions() == 2 ) {
                         // Not tested
-                        fVal = computeBaseFunctionValue1D(j, s);
+                        fVal = computeBaseFunctionValue1D(j, gcoords.at( surfaceIndexes.at(1) ));
                     } else {
                         coord.resize(2);
                         coord.at(1) = gcoords.at( surfaceIndexes.at(1) );
                         coord.at(2) = gcoords.at( surfaceIndexes.at(2) );
                         fVal = computeBaseFunctionValue2D(j + 1, coord);
                     }
-                    temp.at(j+1)=temp.at(j+1)-normalSign*this->g.dotProduct(gcoords)*fVal*gp->giveWeight()*detJ;
+                    temp.at(j+1)=temp.at(j+1) + normalSign*this->g.dotProduct(gcoords)*fVal*gp->giveWeight()*detJ;
                 }
             }
         }
