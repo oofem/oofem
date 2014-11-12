5df98502d59a4856f62b095ab3fa8b48fa9b707a
Milan Jirásek <Milan.Jirasek@fsv.cvut.cz>
2014-11-12 23:03:40 +0100

    Added general anisotropic linear elastic material.

---
+++ src/sm/anisolinearelasticmaterial.C
@@ -0,0 +1,132 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2013   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "linearelasticmaterial.h"
+#include "anisolinearelasticmaterial.h"
+#include "structuralelement.h"
+#include "material.h"
+#include "structuralms.h"
+#include "floatmatrix.h"
+#include "gausspoint.h"
+#include "mathfem.h"
+#include "classfactory.h"
+#include "dynamicinputrecord.h"
+
+namespace oofem {
+REGISTER_Material(AnisotropicLinearElasticMaterial);
+
+IRResultType
+AnisotropicLinearElasticMaterial :: initializeFrom(InputRecord *ir)
+{
+    IRResultType result;                // Required by IR_GIVE_FIELD macro
+    this->LinearElasticMaterial :: initializeFrom(ir);
+
+    // read the stiffness coefficients arranged by rows from the diagonal to the right (21 values)
+    FloatArray stiffness;
+    IR_GIVE_FIELD(ir, stiffness, _IFT_AnisotropicLinearElasticMaterial_stiff);
+    if ( stiffness.giveSize() != 21 ) {
+        OOFEM_ERROR( "Incorrect size of stiff - should be 21, is %d\n", stiffness.giveSize() );
+    }
+
+    // put the stiffness coefficients into a 6x6 matrix
+    int i, j, k = 1;
+    for ( i = 1; i <= 6; i++ ) {
+        stiffmat.at(i, i) = stiffness.at(k++);
+        for ( j = i + 1; j <= 6; j++ ) {
+            stiffmat.at(i, j) = stiffmat.at(j, i) = stiffness.at(k++);
+        }
+    }
+
+    // read the thermal expansion coefficients (3 values)
+    IR_GIVE_FIELD(ir, alpha, _IFT_AnisotropicLinearElasticMaterial_talpha);
+    if ( alpha.giveSize() == 0 ) {
+        alpha.resize(3);
+        alpha.zero();
+    } else if ( alpha.giveSize() != 3 )     {
+        OOFEM_ERROR( "Incorrect size of talpha - should be 0 or 3, is %d\n", alpha.giveSize() );
+    }
+
+    return IRRT_OK;
+}
+
+
+void
+AnisotropicLinearElasticMaterial :: giveInputRecord(DynamicInputRecord &input)
+{
+    Material :: giveInputRecord(input);
+}
+
+double
+AnisotropicLinearElasticMaterial :: give(int aProperty, GaussPoint *gp)
+//
+// Returns the value of the property aProperty (e.g. the Young's modulus 'E') of the receiver.
+//
+{
+    return this->Material :: give(aProperty, gp);
+}
+
+
+void
+AnisotropicLinearElasticMaterial :: give3dMaterialStiffnessMatrix(FloatMatrix &answer,
+                                                                  MatResponseMode mode,
+                                                                  GaussPoint *gp,
+                                                                  TimeStep *tStep)
+//
+// forceElasticResponse ignored - always elastic
+//
+{
+    answer = stiffmat;
+}
+
+
+void
+AnisotropicLinearElasticMaterial :: giveThermalDilatationVector(FloatArray &answer,
+                                                                GaussPoint *gp, TimeStep *tStep)
+//
+// returns a FloatArray(3) of coefficients of thermal dilatation
+//
+{
+    answer = alpha;
+}
+
+
+MaterialStatus *
+AnisotropicLinearElasticMaterial :: CreateStatus(GaussPoint *gp) const
+/*
+ * creates new  material status  corresponding to this class
+ */
+{
+    return new StructuralMaterialStatus(1, this->giveDomain(), gp);
+}
+} // end namespace oofem
