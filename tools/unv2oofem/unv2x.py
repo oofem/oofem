d9c9b6138b2c018a256f37feca748f3b8af735c2
Vít Šmilauer <smilauer@cml.fsv.cvut.cz>
2014-12-04 09:08:00 +0100

    Fixed orientation of triangular elements.

--- tools/unv2oofem/unv2x.py
+++ tools/unv2oofem/unv2x.py
@@ -51,9 +51,9 @@ class UNVParser:
         oofem_elemProp.append(oofem_elementProperties("LIBeam3D2",oofem_elemProp[-1]))
         oofem_elemProp.append(oofem_elementProperties("LIBeam3Dnl",oofem_elemProp[-1]))
         oofem_elemProp.append(oofem_elementProperties("LIBeam3Dnl2",oofem_elemProp[-1]))
-        oofem_elemProp.append(oofem_elementProperties("TrPlaneStress2D", [0,2,1], [[0,2],[2,1],[1,0]],[])) #checked - current numbering of triangle nodes is anti-clockwise, the same orientation as in OOFEM.
-        oofem_elemProp.append(oofem_elementProperties("TrPlaneStress2DXFEM", [0,2,1], [[0,2],[2,1],[1,0]],[])) 
-        #oofem_elemProp.append(oofem_elementProperties("TrPlaneStress2D", [0,1,2], [[0,1],[1,2],[2,0]],[])) #old version of UNV export in SALOME, nodes on triangular elements are numbered clockwise
+        #oofem_elemProp.append(oofem_elementProperties("TrPlaneStress2D", [0,2,1], [[0,2],[2,1],[1,0]],[])) #old version - current numbering of triangle nodes is clockwise, the same orientation as in OOFEM.
+        oofem_elemProp.append(oofem_elementProperties("TrPlaneStress2D", [0,1,2], [[0,1],[1,2],[2,0]],[])) #new version- UNV export in SALOME, nodes on triangular elements are numbered anti-clockwise (NETGEN output)
+        oofem_elemProp.append(oofem_elementProperties("TrPlaneStress2DXFEM", oofem_elemProp[-1]))
         oofem_elemProp.append(oofem_elementProperties("TrplaneStrain",oofem_elemProp[-1]))
         oofem_elemProp.append(oofem_elementProperties("Axisymm3D",oofem_elemProp[-1]))
         oofem_elemProp.append(oofem_elementProperties("Tr1ht",oofem_elemProp[-1]))
