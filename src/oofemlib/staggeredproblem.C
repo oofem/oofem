2bedd02dd455d57274a8cea1d06b587fb8a00bfa
Vit Smilauer <vit.smilauer@fsv.cvut.cz>
2015-10-22 04:23:23 +0200

    Adaptive step length for staggeredproblem.C. Fixed oofemctrlreader.py when missing keyword 'nxfemman'.

--- src/oofemlib/staggeredproblem.C
+++ src/oofemlib/staggeredproblem.C
@@ -380,9 +380,9 @@ StaggeredProblem :: giveNextStep()
     } else {
         if ( this->adaptiveStepLength ) {
             OOFEM_LOG_INFO("\n==================================================================\n");
-            OOFEM_LOG_INFO( "\nAdjusting time step length to: %lf \n\n",  this->giveDeltaT(istep) );
+            OOFEM_LOG_INFO( "\nAdjusting time step length to: %lf \n\n", totalTime - previousStep->giveTargetTime() );
         }
-        currentStep.reset( new TimeStep(istep, this, 1, totalTime, this->giveDeltaT(istep), counter) );
+        currentStep.reset( new TimeStep(istep, this, 1, totalTime, totalTime - previousStep->giveTargetTime(), counter) );
     }
 
     // time and dt variables are set eq to 0 for statics - has no meaning
