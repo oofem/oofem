022dd418dcfe87397cbb2006d9dd658743ee6fc4
Mikael Öhman <micketeer@gmail.com>
2016-04-01 03:56:06 +0200

    Upgraded hexaquadgrid tool

--- tools/rve_generator/hexagrid.py
+++ tools/rve_generator/hexagrid.py
@@ -10,12 +10,19 @@ import rveToolbox
 from numpy import *
 
 def printRVE(rveSampleNumber, rveSize, rvePosition):
+    # 3 nodes along each element axis
+    #elname, w = 'Hex21Stokes', 3
+    #elname, w = 'Q27Space', 3
+    #elname, w = 'LSpace', 2
+    #elname, w = 'QBrick1HT', 3
+    elname, w = 'Brick1HT', 2
+
     nelem = 5*rveSize; # 5 elements per unit cell
 
     rveInclusions = rveToolbox.getInclusionsInBox(rvePosition, rveSize, inclusions)
     print('%d inclusions in RVE'%len(rveInclusions))
 
-    n = nelem*2 + 1;
+    n = nelem*(w-1) + 1;
     X, Y, Z = mgrid[rvePosition[0]:(rvePosition[0]+rveSize):n*1j,
                     rvePosition[1]:(rvePosition[1]+rveSize):n*1j,
                     rvePosition[2]:(rvePosition[2]+rveSize):n*1j ]
@@ -25,46 +32,58 @@ def printRVE(rveSampleNumber, rveSize, rvePosition):
     nC = nX + nY*n + nZ*n*n + 1; # + 1 because of oofems numbering
 
     # Create elements
-    elem = zeros([nelem*nelem*nelem, 27]).astype(int)
+    elem = zeros([nelem*nelem*nelem, w**2]).astype(int)
     emat = zeros([nelem*nelem*nelem]).astype(int)
     for ez in range(0,nelem):
         for ey in range(0,nelem):
             for ex in range(0,nelem):
                 e = ex + ey*nelem + ez*nelem*nelem
 
-                elem[e, 0] = nC[0+ex*2,0+ey*2,2+ez*2]
-                elem[e, 1] = nC[0+ex*2,2+ey*2,2+ez*2]
-                elem[e, 2] = nC[2+ex*2,2+ey*2,2+ez*2]
-                elem[e, 3] = nC[2+ex*2,0+ey*2,2+ez*2]
+                if w == 3:
+                    elem[e, 0] = nC[0+ex*2,0+ey*2,2+ez*2]
+                    elem[e, 1] = nC[0+ex*2,2+ey*2,2+ez*2]
+                    elem[e, 2] = nC[2+ex*2,2+ey*2,2+ez*2]
+                    elem[e, 3] = nC[2+ex*2,0+ey*2,2+ez*2]
             
-                elem[e, 4] = nC[0+ex*2,0+ey*2,0+ez*2]
-                elem[e, 5] = nC[0+ex*2,2+ey*2,0+ez*2]
-                elem[e, 6] = nC[2+ex*2,2+ey*2,0+ez*2]
-                elem[e, 7] = nC[2+ex*2,0+ey*2,0+ez*2]
+                    elem[e, 4] = nC[0+ex*2,0+ey*2,0+ez*2]
+                    elem[e, 5] = nC[0+ex*2,2+ey*2,0+ez*2]
+                    elem[e, 6] = nC[2+ex*2,2+ey*2,0+ez*2]
+                    elem[e, 7] = nC[2+ex*2,0+ey*2,0+ez*2]
             
-                elem[e, 8] = nC[0+ex*2,1+ey*2,2+ez*2]
-                elem[e, 9] = nC[1+ex*2,2+ey*2,2+ez*2]
-                elem[e,10] = nC[2+ex*2,1+ey*2,2+ez*2]
-                elem[e,11] = nC[1+ex*2,0+ey*2,2+ez*2]
+                    elem[e, 8] = nC[0+ex*2,1+ey*2,2+ez*2]
+                    elem[e, 9] = nC[1+ex*2,2+ey*2,2+ez*2]
+                    elem[e,10] = nC[2+ex*2,1+ey*2,2+ez*2]
+                    elem[e,11] = nC[1+ex*2,0+ey*2,2+ez*2]
             
-                elem[e,12] = nC[0+ex*2,1+ey*2,0+ez*2]
-                elem[e,13] = nC[1+ex*2,2+ey*2,0+ez*2]
-                elem[e,14] = nC[2+ex*2,1+ey*2,0+ez*2]
-                elem[e,15] = nC[1+ex*2,0+ey*2,0+ez*2]
+                    elem[e,12] = nC[0+ex*2,1+ey*2,0+ez*2]
+                    elem[e,13] = nC[1+ex*2,2+ey*2,0+ez*2]
+                    elem[e,14] = nC[2+ex*2,1+ey*2,0+ez*2]
+                    elem[e,15] = nC[1+ex*2,0+ey*2,0+ez*2]
             
-                elem[e,16] = nC[0+ex*2,0+ey*2,1+ez*2]
-                elem[e,17] = nC[0+ex*2,2+ey*2,1+ez*2]
-                elem[e,18] = nC[2+ex*2,2+ey*2,1+ez*2]
-                elem[e,19] = nC[2+ex*2,0+ey*2,1+ez*2]
+                    elem[e,16] = nC[0+ex*2,0+ey*2,1+ez*2]
+                    elem[e,17] = nC[0+ex*2,2+ey*2,1+ez*2]
+                    elem[e,18] = nC[2+ex*2,2+ey*2,1+ez*2]
+                    elem[e,19] = nC[2+ex*2,0+ey*2,1+ez*2]
             
-                elem[e,20] = nC[1+ex*2,1+ey*2,2+ez*2]
-                elem[e,21] = nC[1+ex*2,1+ey*2,0+ez*2]
-                elem[e,22] = nC[0+ex*2,1+ey*2,1+ez*2]
-                elem[e,23] = nC[1+ex*2,2+ey*2,1+ez*2]
-                elem[e,24] = nC[2+ex*2,1+ey*2,1+ez*2]
-                elem[e,25] = nC[1+ex*2,0+ey*2,1+ez*2]
+                    elem[e,20] = nC[1+ex*2,1+ey*2,2+ez*2]
+                    elem[e,21] = nC[1+ex*2,1+ey*2,0+ez*2]
+                    elem[e,22] = nC[0+ex*2,1+ey*2,1+ez*2]
+                    elem[e,23] = nC[1+ex*2,2+ey*2,1+ez*2]
+                    elem[e,24] = nC[2+ex*2,1+ey*2,1+ez*2]
+                    elem[e,25] = nC[1+ex*2,0+ey*2,1+ez*2]
             
-                elem[e,26] = nC[1+ex*2,1+ey*2,1+ez*2]
+                    elem[e,26] = nC[1+ex*2,1+ey*2,1+ez*2]
+                else:
+                    elem[e, 0] = nC[0+ex*1,0+ey*1,1+ez*1]
+                    elem[e, 1] = nC[0+ex*1,1+ey*1,1+ez*1]
+                    elem[e, 2] = nC[1+ex*1,1+ey*1,1+ez*1]
+                    elem[e, 3] = nC[1+ex*1,0+ey*1,1+ez*1]
+            
+                    elem[e, 4] = nC[0+ex*1,0+ey*1,0+ez*1]
+                    elem[e, 5] = nC[0+ex*1,1+ey*1,0+ez*1]
+                    elem[e, 6] = nC[1+ex*1,1+ey*1,0+ez*1]
+                    elem[e, 7] = nC[1+ex*1,0+ey*1,0+ez*1]
+
 
                 # Check if center coordinate is within some sphere:
                 emat[e] = 1
@@ -80,16 +99,22 @@ def printRVE(rveSampleNumber, rveSize, rvePosition):
 
 
     # Write the input file
-    fname = 'hexstokes_size%d_sample%d'%(rveSize,rveSampleNumber);
+    fname = 'hex_size%d_sample%d'%(rveSize,rveSampleNumber);
     f = open(fname+'.in','w')
     print(fname+'.out', file=f)
-    print('Hex grid for incompressible stokes flow', file=f)
-    print('StokesFlow nsteps 1 rtolf 1e-6 lstype 3 smtype 7 nonlinform 1 nmodules 0', file=f)
-    #print('Nonlinearstatic nsteps 1 rtolf 1e-6 lstype 3 smtype 7 controlmode 1 nmodules 1', file=f)
-    #print('VTKXML tstep_all domain_all primvars 2 4 5 cellvars 1 46', file=f)
-    #print('VTKXML tstep_all domain_all primvars 1 1 cellvars 2 46 4', file=f)
-    print('Domain 3dIncompFlow', file=f)
-    #print('Domain 3d', file=f)
+    print('Hex grid RVE', file=f)
+    if elname == 'Hexa21Stokes':
+        print('StokesFlow nsteps 1 rtolf 1e-6 lstype 3 smtype 7 nonlinform 1 nmodules 0', file=f)
+        print('VTKXML tstep_all domain_all primvars 2 4 5 cellvars 1 46', file=f)
+    elif elname == 'LSpace' or elname == 'Q27Space':
+        print('StaticStructural nsteps 1 rtolf 1e-6 lstype 3 smtype 7 nmodules 1', file=f)
+        print('VTKXML tstep_all domain_all primvars 1 1 cellvars 2 46 4', file=f)
+    else:
+        print('StationaryProblem nsteps 1 rtolf 1e-6 lstype 3 smtype 7 nmodules 1', file=f)
+        print('VTKXML tstep_all domain_all primvars 1 1 cellvars 2 46 4', file=f)
+    print('StaticStructural nsteps 1 rtolf 1e-6 lstype 3 smtype 7 nmodules 1', file=f)
+    print('VTKXML tstep_all domain_all primvars 1 1 cellvars 2 46 4', file=f)
+    print('Domain 3d', file=f)
     print('OutputManager', file=f)
     print('ndofman', n*n*n, 'nelem', nelem*nelem*nelem, 'ncrosssect 1 nmat 2 nbc 4 nic 0 nltf 1 nset 4', file=f)
 
@@ -107,25 +132,25 @@ def printRVE(rveSampleNumber, rveSize, rvePosition):
             for ex in range(0,nelem):
                 e = ex + ey*nelem + ez*nelem*nelem
                 q = elem[e];
-                elname = 'Hexa21Stokes'
-                #elname = 'Q27Space'
-                print(elname, e+1, 'mat', emat[e], 'crossSect 1 nodes',size(q),
-                    q[0],q[1],q[2],q[3],q[4],q[5],q[6],q[7],q[8],q[9],q[10],q[11],q[12],q[13],q[14],q[15],q[16],
-                    q[17],q[18],q[19],q[20],q[21],q[22],q[23],q[24],q[25],q[26], 
-                    file=f)
+                print(elname, e+1, 'crosssect', emat[e], ' nodes',size(q), *q, file=f)
 
-    #print('EmptyCS 1', file=f)
-    print('SimpleCS 1 thick 0.2', file=f)
-    print('NewtonianFluid 1 d 1 mu 1', file=f)
-    print('NewtonianFluid 2 d 1 mu 5', file=f)
-    #print('IsoLE 1 d 1 E 1 n 0.45 talpha 0', file=f)
-    #print('IsoLE 2 d 1 E 5 n 0.45 talpha 0', file=f)
+    if elname == 'Hexa21Stokes':
+        print('FluidCS 1 mat 1', file=f)
+        print('FluidCS 1 mat 2', file=f)
+    elif elname == 'LSpace' or elname == 'Q27Space':
+        print('SimpleCS 1 material 1', file=f)
+        print('SimpleCS 1 material 2', file=f)
+    else:
+        print('SimpleTransportCS 1 mat 1', file=f)
+        print('SimpleTransportCS 2 mat 2', file=f)
+
+    print('IsoLE 1 d 1 E 1 n 0.3 talpha 0', file=f)
+    print('IsoLE 2 d 1 E 5 n 0.3 talpha 0', file=f)
     #print('MixedGradientPressureNeumann 1 loadTimeFunction 1 set 1 devgradient 6 0 0 0 1 1 1 pressure 0', file=f)
-    print('MixedGradientPressureDirichlet 1 loadTimeFunction 1 set 1 dofs 6 1 2 3 7 8 9 devgradient 6 0 0 0 1 1 1 pressure 0', file=f)
+    #print('MixedGradientPressureDirichlet 1 loadTimeFunction 1 set 1 dofs 6 1 2 3 7 8 9 devgradient 6 0 0 0 1 1 1 pressure 0', file=f)
     #print('PrescribedGradient 1 loadTimeFunction 1 set 1 dofs 6 1 2 3 7 8 9 gradient 3 3 {0 0 1; 0 0 0; 1 0 0}', file=f)
-    print('BoundaryCondition 2 d 0.0 loadTimeFunction 1 dofs 6 1 2 3 7 8 9 set 0', file=f)
-    print('BoundaryCondition 3 d 1.0 loadTimeFunction 1 dofs 6 1 2 3 7 8 9 set 0', file=f)
-    print('DeadWeight 4 loadTimeFunction 1 components 3 0 0 -1 set 0', file=f)
+    print('BoundaryCondition 2 loadTimeFunction 1 values 3 0 0 0 dofs 3 1 2 3 set 0', file=f)
+    print('BoundaryCondition 3 loadTimeFunction 1 values 3 0 0 0 dofs 3 1 2 3 set 0', file=f)
     print('ConstantFunction 1 f(t) 1.0', file=f)
     print('Set 1 elementboundaries', 2*6*nelem*nelem, end=' ', file=f)
 
