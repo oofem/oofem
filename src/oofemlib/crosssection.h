059c3f286e1adef5feb91aa0420e604c59eca13b
Mikael Öhman <micketeer@gmail.com>
2016-07-06 20:43:26 +0200

    Added ability to check if CS has prop.

--- src/oofemlib/crosssection.h
+++ src/oofemlib/crosssection.h
@@ -127,6 +127,11 @@ public:
     int giveSetNumber() const { return this->setNumber; }
 
     /**
+     * Returns true if the dictionary contains the requested property.
+     * @param a Id of requested property.
+     */
+    virtual bool hasProperty(CrossSectionProperty a);
+    /**
      * Returns the value of cross section property at given point.
      * The default implementation assumes constant properties stored in propertyDictionary.
      * @param a Id of requested property.
