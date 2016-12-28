0fbd796c4f70ecd420277806b61faf3b9ddd191d
Bořek Patzák <borek.patzak@gmail.com>
2016-12-28 19:57:34 +0100

    Extended RigidArmNode to support different lcs in master and slave

--- src/oofemlib/rigidarmnode.h
+++ src/oofemlib/rigidarmnode.h
@@ -49,18 +49,21 @@ class IntArray;
 
 /**
  * Class implementing node connected to other node (master) using rigid arm in finite element mesh.
- * Rigid arm node possess no degrees of freedom - all dofs are mapped to master dofs.
- * The Rigid arm node supports not only slave dofs mapped to master
- * but also some dofs can be primary dofs. Introduced masterDofMask allowing to
- * distinguish between primary and mapped (slave) dofs. The primary DOFs can have their own BCs, ICs.
+ * The Rigid arm node supports not only slave dofs mapped to master throug rigid arm connection
+ * but also some dofs can be primary dofs (specified by masterDofMask attribute)
+ * The primary DOFs can have their own BCs, ICs.
  *
  * The introduction of rigid arm connected nodes allows to avoid very stiff elements used
  * for modeling the rigid-arm connection. The rigid arm node maps its dofs to master dofs
  * using simple transformations (small rotations are assumed). Therefore, the contribution
  * to rigid arm node are localized directly to master related equations.
- * The rigid arm node can not have its own boundary or initial conditions,
+ * The rigid arm node can not have its own boundary or initial conditions on linked DOFs,
  * they are determined completely from master dof conditions.
- * The local coordinate system in slave is not supported in current implementation, the global cs applies.
+ *
+ * Updated by bp: The local coordinate system in slave is supported in current implementation, 
+ * the coordinate system in master and slave can be different. 
+ * If no lcs is set, global one is assumed.the global cs applies.
+ *
  * On the other hand, rigid arm node can be loaded independently of master.
  * The transformation for DOFs and load is not orthogonal - the inverse transformation can
  * not be constructed by transposition. Because of time consuming inversion, methods
