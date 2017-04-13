a35d8437ebfba5a4cce2bc5ec71f0f7b8863c63e
Martin Horak <nitramkaroh@seznam.cz>
2017-04-13 10:03:04 +0200

    Converting matrix from C 0-based to Fortran 1-based notation in pardisoprojectorgsolver

--- src/oofemlib/pardisoprojectorgsolver.C
+++ src/oofemlib/pardisoprojectorgsolver.C
@@ -57,10 +57,10 @@ NM_Status PardisoProjectOrgSolver :: solve(SparseMtrx &A, FloatArray &b, FloatAr
     x.resize(neqs);
 
     int mtype = -2;        // Real symmetric positive definite matrix
-    CompCol *mat = dynamic_cast< SymCompCol * >(&A);
+    CompCol *mat = dynamic_cast< SymCompCol * >(& A);
     if ( !mat ) {
         mtype = 11;        // Real unsymmetric matrix
-        mat = dynamic_cast< CompCol * >(&A);
+        mat = dynamic_cast< CompCol * >(& A);
         if ( !mat ) {
             OOFEM_ERROR("CompCol matrix needed for Pardiso solver");
         }
@@ -71,10 +71,27 @@ NM_Status PardisoProjectOrgSolver :: solve(SparseMtrx &A, FloatArray &b, FloatAr
         return NM_Success;
     }
 
-    const int *ia = mat->giveColPtr().givePointer();
-    const int *ja = mat->giveRowIndex().givePointer();
+    int *ia = mat->giveColPtr().givePointer();
+    int *ja = mat->giveRowIndex().givePointer();
     double *a = mat->giveValues().givePointer();
-    
+
+
+    /* -------------------------------------------------------------------- */
+    /* ..  Convert matrix from 0-based C-notation to Fortran 1-based        */
+    /*     notation.                                                        */
+    /* -------------------------------------------------------------------- */
+    int n =  mat->giveNumberOfRows();
+    for ( int i = 0; i < n + 1; i++ ) {
+        ia [ i ] += 1;
+    }
+    int nnz = ia [ n ];
+    for ( int i = 0; i < nnz; i++ ) {
+        ja [ i ] += 1;
+    }
+
+
+
+
     Timer timer;
     timer.startTimer();
 
@@ -84,7 +101,7 @@ NM_Status PardisoProjectOrgSolver :: solve(SparseMtrx &A, FloatArray &b, FloatAr
     // Internal solver memory pointer pt,
     // 32-bit: int pt[64]; 64-bit: long int pt[64]
     // or void *pt[64] should be OK on both architectures
-    void *pt[64]; 
+    void *pt [ 64 ];
 
     // Pardiso control parameters.
     IntArray iparm(64); ///@todo pardisoinit seems to write outside this array
@@ -93,25 +110,25 @@ NM_Status PardisoProjectOrgSolver :: solve(SparseMtrx &A, FloatArray &b, FloatAr
 
     double ddum = 0.;           // Double dummy
     int idum = 0;              // Integer dummy.
-    
+
     // Setup Pardiso control parameters
     /* -------------------------------------------------------------------- */
     error = 0;
     int solver = 0; // sparse direct
-    pardisoinit(pt, &mtype, &solver, iparm.givePointer(), dparm.givePointer(), &error);  // INITIALIZATION!
+    pardisoinit(pt, & mtype, & solver, iparm.givePointer(), dparm.givePointer(), & error);  // INITIALIZATION!
     if ( error != 0 ) {
         OOFEM_WARNING("Error during pardiso init, error code: %d", error);
         return NM_NoSuccess;
     }
-     // Settings are here:
+    // Settings are here:
     // https://software.intel.com/en-us/articles/pardiso-parameter-table#table2
 
-    iparm[0] = 1;
+    iparm [ 0 ] = 1;
     ///@todo I might be misunderstanding something, but this iterative solver still does a full factorization. No options for incomplete factorizations.
     //iparm[4-1] = 32; // 10*L + K. K = 1 implies CGS (instead of LU), K = 2 implies CG. L specifies exponent tolerance.
-    iparm[8-1] = 2;       /* Max numbers of iterative refinement steps. */ ///@todo I have no idea if this is suitable value. Examples use 2. / Mikael
-    iparm[12-1] = 2; // Transpose (we have a CSC matrix representation here instead of the expected CSR)
-    iparm[35-1] = 1; // 1 implies 0-indexing
+    iparm [ 8 - 1 ] = 2; /* Max numbers of iterative refinement steps. */  ///@todo I have no idea if this is suitable value. Examples use 2. / Mikael
+    iparm [ 12 - 1 ] = 2; // Transpose (we have a CSC matrix representation here instead of the expected CSR)
+    iparm [ 35 - 1 ] = 1; // 1 implies 0-indexing
     //iparm[27-1] = 1; // Checks the matrix (only in MKL)
     ///@todo This is not included in the table of options for some reason!
 
@@ -124,26 +141,26 @@ NM_Status PardisoProjectOrgSolver :: solve(SparseMtrx &A, FloatArray &b, FloatAr
     /* ..  Reordering and Symbolic Factorization.  This step also allocates */
     /*     all memory that is necessary for the factorization.              */
     /* -------------------------------------------------------------------- */
-    phase = 11; 
+    phase = 11;
 
-    pardiso(pt, &maxfct, &mnum, &mtype, &phase, &neqs, 
-            a, (int*)ia, (int*)ja,
-            &idum, &nrhs, iparm.givePointer(), &msglvl, &ddum, &ddum, &error, dparm.givePointer());   // FACTORIZATION!
+    pardiso( pt, & maxfct, & mnum, & mtype, & phase, & neqs,
+             a, ( int * ) ia, ( int * ) ja,
+             & idum, & nrhs, iparm.givePointer(), & msglvl, & ddum, & ddum, & error, dparm.givePointer() ); // FACTORIZATION!
 
     if ( error != 0 ) {
         OOFEM_WARNING("Error during symbolic factorization: %d", error);
         return NM_NoSuccess;
     }
-    OOFEM_LOG_DEBUG("Reordering completed: %d nonzero factors, %d factorization MFLOPS\n", iparm[17-1], iparm[18-1]);
-   
+    OOFEM_LOG_DEBUG("Reordering completed: %d nonzero factors, %d factorization MFLOPS\n", iparm [ 17 - 1 ], iparm [ 18 - 1 ]);
+
     /* -------------------------------------------------------------------- */
     /* ..  Numerical factorization.                                         */
-    /* -------------------------------------------------------------------- */    
+    /* -------------------------------------------------------------------- */
     phase = 22;
 
-    pardiso(pt, &maxfct, &mnum, &mtype, &phase, &neqs,
-            a, (int*)ia, (int*)ja,
-            &idum, &nrhs, iparm.givePointer(), &msglvl, &ddum, &ddum, &error, dparm.givePointer());
+    pardiso( pt, & maxfct, & mnum, & mtype, & phase, & neqs,
+             a, ( int * ) ia, ( int * ) ja,
+             & idum, & nrhs, iparm.givePointer(), & msglvl, & ddum, & ddum, & error, dparm.givePointer() );
 
     if ( error != 0 ) {
         OOFEM_WARNING("ERROR during numerical factorization: %d", error);
@@ -151,31 +168,45 @@ NM_Status PardisoProjectOrgSolver :: solve(SparseMtrx &A, FloatArray &b, FloatAr
     }
     OOFEM_LOG_DEBUG("Factorization completed ...\n");
 
-    /* -------------------------------------------------------------------- */    
+    /* -------------------------------------------------------------------- */
     /* ..  Back substitution and iterative refinement.                      */
-    /* -------------------------------------------------------------------- */    
+    /* -------------------------------------------------------------------- */
     phase = 33;
 
-    pardiso(pt, &maxfct, &mnum, &mtype, &phase, &neqs,
-            a, (int*)ia, (int*)ja,
-            &idum, &nrhs, iparm.givePointer(), &msglvl, b.givePointer(), x.givePointer(), &error, dparm.givePointer());
+    pardiso( pt, & maxfct, & mnum, & mtype, & phase, & neqs,
+             a, ( int * ) ia, ( int * ) ja,
+             & idum, & nrhs, iparm.givePointer(), & msglvl, b.givePointer(), x.givePointer(), & error, dparm.givePointer() );
 
-    printf("iparm(20) = %d\n", iparm[20]);
+    printf("iparm(20) = %d\n", iparm [ 20 ]);
     if ( error != 0 ) {
-        OOFEM_WARNING("ERROR during solution: %d, iparm(20) = %d", error, iparm[20-1]);
+        OOFEM_WARNING("ERROR during solution: %d, iparm(20) = %d", error, iparm [ 20 - 1 ]);
         return NM_NoSuccess;
     }
 
     OOFEM_LOG_DEBUG("Solve completed ... \n");
 
-    /* -------------------------------------------------------------------- */    
+
+    /* -------------------------------------------------------------------- */
+    /* ..  Convert matrix back to 0-based C-notation.                       */
+    /* -------------------------------------------------------------------- */
+    for ( int i = 0; i < n + 1; i++ ) {
+        ia [ i ] -= 1;
+    }
+    for ( int i = 0; i < nnz; i++ ) {
+        ja [ i ] -= 1;
+    }
+
+
+    /* -------------------------------------------------------------------- */
     /* ..  Termination and release of memory.                               */
-    /* -------------------------------------------------------------------- */    
+    /* -------------------------------------------------------------------- */
+
+
     phase = -1;                 /* Release internal memory. */
 
-    pardiso(pt, &maxfct, &mnum, &mtype, &phase,
-            &neqs, &ddum, &idum, &idum, &idum, &nrhs,
-            iparm.givePointer(), &msglvl, &ddum, &ddum, &error,dparm.givePointer());
+    pardiso( pt, & maxfct, & mnum, & mtype, & phase,
+             & neqs, & ddum, & idum, & idum, & idum, & nrhs,
+             iparm.givePointer(), & msglvl, & ddum, & ddum, & error, dparm.givePointer() );
 
     timer.stopTimer();
     OOFEM_LOG_INFO( "MKLPardisoSolver:  User time consumed by solution: %.2fs\n", timer.getUtime() );
--- src/oofemlib/pardisoprojectorgsolver.h
+++ src/oofemlib/pardisoprojectorgsolver.h
@@ -37,22 +37,21 @@
 
 #include "sparselinsystemnm.h"
 /* PARDISO prototype. */
-extern "C" void pardisoinit (void   *, int    *,   int *, int *, double *, int *);
-extern "C" void pardiso     (void   *, int    *,   int *, int *,    int *, int *, 
-                  double *, int    *,    int *, int *,   int *, int *,
-                     int *, double *, double *, int *, double *);
-extern "C" void pardiso_chkmatrix  (int *, int *, double *, int *, int *, int *);
-extern "C" void pardiso_chkvec     (int *, int *, double *, int *);
-extern "C" void pardiso_printstats (int *, int *, double *, int *, int *, int *,
-                           double *, int *);
+extern "C" void pardisoinit(void *, int *,   int *, int *, double *, int *);
+extern "C" void pardiso(void *, int *,   int *, int *,    int *, int *,
+                        double *, int *,    int *, int *,   int *, int *,
+                        int *, double *, double *, int *, double *);
+extern "C" void pardiso_chkmatrix(int *, int *, double *, int *, int *, int *);
+extern "C" void pardiso_chkvec(int *, int *, double *, int *);
+extern "C" void pardiso_printstats(int *, int *, double *, int *, int *, int *,
+                                   double *, int *);
 
 namespace oofem {
-
 /**
  * Implements the solution of linear system of equation in the form @f$ A\cdot x=b @f$ using solvers
  * from Pardiso-project.org. Adapted from mklpardisosolver implementation by M. Ohman
  * Only works with the CSC (compressed sparse column) sparse matrix implementation.
- * 
+ *
  */
 class OOFEM_EXPORT PardisoProjectOrgSolver : public SparseLinearSystemNM
 {
@@ -62,7 +61,7 @@ public:
      * @param d Domain which solver belongs to.
      * @param m Engineering model which solver belongs to.
      */
-    PardisoProjectOrgSolver(Domain * d, EngngModel * m);
+    PardisoProjectOrgSolver(Domain *d, EngngModel *m);
 
     virtual ~PardisoProjectOrgSolver();
 
