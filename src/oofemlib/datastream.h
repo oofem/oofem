5c5b50d98cfb323a7d49138c6b6b099106fb3290
Bořek Patzák <borek.patzak@gmail.com>
2017-11-29 10:52:08 +0100

    added std:: namespace resolution to runtime_error

--- src/oofemlib/datastream.h
+++ src/oofemlib/datastream.h
@@ -139,7 +139,7 @@ public:
     {
     public:
         std::string filename;
-        CantOpen(std::string file): runtime_error("can't open file"), filename(std::move(file)) {}
+        CantOpen(std::string file): std::runtime_error("can't open file"), filename(std::move(file)) {}
     };
 
 private:
