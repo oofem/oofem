b42f6598e141e3bfdc1684fac5bf92dfdf0c53ec
Bořek Patzák <borek.patzak@gmail.com>
2023-03-24 13:51:15 +0100

    Making problem input reader to finish to check emodel input record

--- src/oofemlib/util.C
+++ src/oofemlib/util.C
@@ -167,6 +167,7 @@ std::unique_ptr<EngngModel> InstanciateProblem(DataReader &dr, problemMode mode,
     }
 
     problem->instanciateYourself( dr, *emodelir, dataOutputFileName.c_str(), desc.c_str() );
+    emodelir->finish();
 
     return problem;
 }
