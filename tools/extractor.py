d35371fe177cac4ab403eb58edc0afe5c1e06741
pedroskop <petr.havlasek@gmail.com>
2023-04-07 21:48:09 +0200

    Fixing bug in extractor.py to allow exporting positive values from IP starting with "+" (bug fix by B. Patzak)

--- tools/extractor.py
+++ tools/extractor.py
@@ -321,8 +321,8 @@ def check_element_rec (context, kwd, line):
         if ((rec[0]=='er') and timeflag and (context.recnumber == rec[2]) and
             (context.recirule==rec[3]) and (context.recgpnum == rec[4])):
             #print ( "Found er: looking for ",rec[5],"in ", line )
-            
-            match=re.search(rec[5]+'\s*((([-]*\d+(\.\d+)?(e[+-]\d+)?)\s*)+)', line)
+
+            match=re.search(rec[5]+'\s*((([+-]*\d+(\.\d+)?(e[+-]\d+)?)\s*)+)', line)
             if match:
                 # print ( "match: ",match.group(1) )
                 context.recVal[irec]=re.split('\s+',match.group(1))[rec[6]-1]
