11de568942515163bf4dcc3fcf4525ec6fa751a9
Borek Patzak <borek.patzak@gmail.com>
2023-11-05 23:08:14 +0100

    Improving version info (git url, branch and hash added)

--- src/main/main.C
+++ src/main/main.C
@@ -371,7 +371,7 @@ void oofem_print_help()
 
 void oofem_print_version()
 {
-    printf("\n%s (%s, %s)\nof %s on %s (Git Hash: %s)\n", PRG_VERSION, HOST_TYPE, MODULE_LIST, __DATE__, HOST_NAME, OOFEM_GIT_HASH);
+    printf("\n%s (%s, %s)\nGit RepoURL: %s\n    Branch: %s, Hash: %s\n", PRG_VERSION, HOST_TYPE, MODULE_LIST, OOFEM_GIT_REPOURL, OOFEM_GIT_BRANCH, OOFEM_GIT_HASH);
     oofem_print_epilog();
 }
 
