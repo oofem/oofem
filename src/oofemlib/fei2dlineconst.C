5a1724d07b6a427967ea791748d8b2ad6f7d0f07
Borek Patzak <borek.patzak@gmail.com>
2025-02-10 21:54:32 +0100

    Adding fei2dlineconst interpolation

---
+++ src/oofemlib/fei2dlineconst.C
@@ -0,0 +1,156 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2013   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "fei2dlineconst.h"
+#include "mathfem.h"
+#include "floatmatrix.h"
+#include "floatarray.h"
+#include "floatarrayf.h"
+#include "gaussintegrationrule.h"
+
+namespace oofem {
+
+
+void FEI2dLineConst :: evalN(FloatArray &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo) const
+{
+    answer.resize(1);
+    answer.at(1) = 1.0;
+}
+
+double FEI2dLineConst :: evaldNdx(FloatMatrix &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo) const
+{
+    answer.clear();
+    return 0.;
+}
+
+void FEI2dLineConst :: local2global(FloatArray &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo) const
+{
+    FloatArray n(2);
+    double xi = lcoords(0);
+    n.at(1) = ( 1. - xi ) * 0.5;
+    n.at(2) = ( 1. + xi ) * 0.5;
+    answer.resize( max(xind, yind) );
+    answer.zero();
+    answer.at(xind) = n(0) * cellgeo.giveVertexCoordinates(1).at(xind) +
+                      n(1) * cellgeo.giveVertexCoordinates(2).at(xind);
+    answer.at(yind) = n(0) * cellgeo.giveVertexCoordinates(1).at(yind) +
+                      n(1) * cellgeo.giveVertexCoordinates(2).at(yind);
+}
+
+int FEI2dLineConst :: global2local(FloatArray &answer, const FloatArray &gcoords, const FEICellGeometry &cellgeo) const
+{
+    double x2_x1 = cellgeo.giveVertexCoordinates(2).at(xind) - cellgeo.giveVertexCoordinates(1).at(xind);
+    double y2_y1 = cellgeo.giveVertexCoordinates(2).at(yind) - cellgeo.giveVertexCoordinates(1).at(yind);
+
+    // Projection of the global coordinate gives the value interpolated in [0,1].
+    double dx = gcoords(0) - cellgeo.giveVertexCoordinates(1).at(xind);
+    double dy = gcoords(1) - cellgeo.giveVertexCoordinates(1).at(yind);
+    double xi = (x2_x1) ? ( sqrt( dx*dx*(1 + (y2_y1 / x2_x1)*(y2_y1 / x2_x1) )) ) / ( sqrt(x2_x1 * x2_x1 + y2_y1 * y2_y1) ) : sqrt(dy*dy) / ( sqrt(x2_x1 * x2_x1 + y2_y1 * y2_y1) );
+    // Map to [-1,1] domain.
+    xi = xi * 2 - 1;
+
+    answer.resize(1);
+    answer(0) = clamp(xi, -1., 1.);
+    return false;
+}
+
+void FEI2dLineConst :: edgeEvaldNds(FloatArray &answer, int iedge,
+                                  const FloatArray &lcoords, const FEICellGeometry &cellgeo) const
+{
+    answer.resize(1);
+    answer.zero();
+}
+
+double FEI2dLineConst :: edgeEvalNormal(FloatArray &normal, int iedge, const FloatArray &lcoords, const FEICellGeometry &cellgeo) const
+{
+    normal.resize(2);
+    normal.at(1) = cellgeo.giveVertexCoordinates(2).at(yind) - cellgeo.giveVertexCoordinates(1).at(yind);
+    normal.at(2) = -( cellgeo.giveVertexCoordinates(2).at(xind) - cellgeo.giveVertexCoordinates(1).at(xind) );
+    return normal.normalize() * 0.5;
+}
+
+double FEI2dLineConst :: giveTransformationJacobian(const FloatArray &lcoords, const FEICellGeometry &cellgeo) const
+{
+    double x2_x1 = cellgeo.giveVertexCoordinates(2).at(xind) - cellgeo.giveVertexCoordinates(1).at(xind);
+    double y2_y1 = cellgeo.giveVertexCoordinates(2).at(yind) - cellgeo.giveVertexCoordinates(1).at(yind);
+    return sqrt(x2_x1 * x2_x1 + y2_y1 * y2_y1) / 2.0;
+}
+
+IntArray FEI2dLineConst :: boundaryEdgeGiveNodes(int boundary, Element_Geometry_Type egt, bool includeHierarchical) const
+{
+    return {1, 2};
+}
+
+IntArray FEI2dLineConst :: computeLocalEdgeMapping(int iedge) const
+{
+    if ( iedge != 1 ) {
+        OOFEM_ERROR("wrong egde number (%d)", iedge);
+    }
+
+    return {1, 2};
+}
+
+void FEI2dLineConst :: edgeEvalN(FloatArray &answer, int iedge, const FloatArray &lcoords, const FEICellGeometry &cellgeo) const
+{
+    this->evalN(answer, lcoords, cellgeo);
+}
+
+double FEI2dLineConst :: edgeComputeLength(const IntArray &edgeNodes, const FEICellGeometry &cellgeo) const
+{
+    double x2_x1 = cellgeo.giveVertexCoordinates(2).at(xind) - cellgeo.giveVertexCoordinates(1).at(xind);
+    double y2_y1 = cellgeo.giveVertexCoordinates(2).at(yind) - cellgeo.giveVertexCoordinates(1).at(yind);
+    return sqrt(x2_x1 * x2_x1 + y2_y1 * y2_y1);
+}
+
+double FEI2dLineConst :: evalNXIntegral(int iEdge, const FEICellGeometry &cellgeo) const
+{
+    const auto &node1 = cellgeo.giveVertexCoordinates(1);
+    double x1 = node1.at(xind);
+    double y1 = node1.at(yind);
+
+    const auto &node2 = cellgeo.giveVertexCoordinates(2);
+    double x2 = node2.at(xind);
+    double y2 = node2.at(yind);
+
+    return x2 * y1 - x1 * y2;
+}
+
+std::unique_ptr<IntegrationRule> FEI2dLineConst :: giveIntegrationRule(int order, Element_Geometry_Type egt) const
+{
+    auto iRule = std::make_unique<GaussIntegrationRule>(1, nullptr);
+    int points = iRule->getRequiredNumberOfIntegrationPoints(_Line, order + 0);
+    iRule->SetUpPointsOnLine(points, _Unknown);
+    return std::move(iRule);
+}
+} // end namespace oofem
---
+++ src/oofemlib/fei2dlineconst.h
@@ -0,0 +1,90 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2013   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef fei2dlineconst_h
+#define fei2dlineconst_h
+
+#include "feinterpol2d.h"
+
+namespace oofem {
+/**
+ * Class representing a 2d line with linear interpolation.
+ * @todo{Some more routines to be implemented here}
+ * @author Mikael Öhman
+ */
+class OOFEM_EXPORT FEI2dLineConst : public FEInterpolation2d
+{
+public:
+    FEI2dLineConst(int ind1, int ind2) : FEInterpolation2d(0, ind1, ind2) { }
+
+    integrationDomain giveIntegrationDomain(const Element_Geometry_Type) const override { return _Line; }
+    const Element_Geometry_Type giveGeometryType() const override { return EGT_line_1; }
+    const Element_Geometry_Type giveBoundaryGeometryType(int boundary) const override { return EGT_point; } 
+
+    integrationDomain giveBoundaryIntegrationDomain(int ib, const Element_Geometry_Type) const override { return _Point; }
+    integrationDomain giveBoundarySurfaceIntegrationDomain(int isurf, const Element_Geometry_Type) const override { return _UnknownIntegrationDomain; }
+    integrationDomain giveBoundaryEdgeIntegrationDomain(int iedge, const Element_Geometry_Type) const override { return _Line; }
+
+    double giveArea(const FEICellGeometry &cellgeo) const override { return 0.0; }
+
+    void local2global(FloatArray &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo) const override;
+    int global2local(FloatArray &answer, const FloatArray &gcoords, const FEICellGeometry &cellgeo) const override;
+
+    int giveNumberOfEdges(const Element_Geometry_Type) const override { return 1; }
+    void giveCellDofMans(IntArray& nodes, IntArray& internalDofMans, Element* elem) const override {nodes={};internalDofMans={1};}
+
+
+    // "Bulk"
+    void evalN(FloatArray &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo) const override;
+    double evaldNdx(FloatMatrix &answer, const FloatArray &lcoords, const FEICellGeometry &cellgeo) const override;
+    double giveTransformationJacobian(const FloatArray &lcoords, const FEICellGeometry &cellgeo) const override;
+
+    // Edge (same as bulk for this type, so they are all ignored) (perhaps do it the other way around?).
+    IntArray boundaryEdgeGiveNodes(int boundary, const Element_Geometry_Type, bool includeHierarchical=false) const override;
+    IntArray computeLocalEdgeMapping(int iedge) const override;
+    void edgeEvalN(FloatArray &answer, int iedge, const FloatArray &lcoords, const FEICellGeometry &cellgeo) const override;
+    double edgeEvalNormal(FloatArray &normal, int iedge, const FloatArray &lcoords, const FEICellGeometry &cellgeo) const override;
+    void edgeEvaldNds(FloatArray &answer, int iedge, const FloatArray &lcoords, const FEICellGeometry &cellgeo) const override;
+    void edgeLocal2global(FloatArray &answer, int iedge, const FloatArray &lcoords, const FEICellGeometry &cellgeo) const override { }
+    double evalNXIntegral(int iEdge, const FEICellGeometry &cellgeo) const override;
+
+    int giveNumberOfNodes(const Element_Geometry_Type) const override { return 2; }
+
+    std::unique_ptr<IntegrationRule> giveIntegrationRule(int order, const Element_Geometry_Type) const override;
+
+protected:
+    double edgeComputeLength(const IntArray &edgeNodes, const FEICellGeometry &cellgeo) const;
+};
+} // end namespace oofem
+#endif // fei2dlineconst_h
