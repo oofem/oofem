cbb113961e53db2bd000e0a4d48c11e28ddabb9b
Borek Patzak <borek.patzak@gmail.com>
2025-03-20 22:55:32 +0100

    Fixing duplicate declaration in oofemutil.h

--- bindings/python/oofemutil.h
+++ bindings/python/oofemutil.h
@@ -260,7 +260,6 @@ py::object trPlaneStress2d(py::args args, py::kwargs &kw) { return createElement
 py::object planeStress2d(py::args args, py::kwargs &kw) { return createElementOfType("planestress2d",args,kw); }
 py::object linquad3dplanestress(py::args args, py::kwargs &kw) { return createElementOfType("linquad3dplanestress",args,kw); }
 py::object qPlaneStress2d(py::args args, py::kwargs &kw) { return createElementOfType("qplanestress2d",args,kw); }
-py::object trPlaneStress2d(py::args args, py::kwargs &kw) { return createElementOfType("trplanestress2d",args,kw); }
 py::object qTrPlStr(py::args args, py::kwargs &kw) { return createElementOfType("qtrplstr",args,kw); }
 py::object trPlaneStrRot(py::args args, py::kwargs &kw) { return createElementOfType("trplanestrrot",args,kw); }
 py::object trPlaneStressRotAllman(py::args args, py::kwargs &kw) { return createElementOfType("trplanestressrotallman",args,kw); }
