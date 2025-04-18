63e139fbdb3532a8a0b8e31cabfd36b95343827f
Borek Patzak <borek.patzak@gmail.com>
2025-04-18 14:17:46 +0200

    util.py: adding utility plot2d function

--- bindings/python/util.py
+++ bindings/python/util.py
@@ -1,4 +1,8 @@
 import oofempy
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+
 
 #
 # Initializes domain with given sequences of nodes, elements, .....
@@ -39,3 +43,36 @@ def setupDomain (domain, nodes, elems, css, mats, bcs, ics, ltfs, sets):
        
     return
 
+def plot2D(d, field=None, fieldValueIndex=0, tstep=None):
+    x=[];
+    y=[];
+    z=[];
+    elements=[];
+    for i in range(1,d.giveNumberOfDofManagers()+1):
+        coords = d.giveDofManager(i).giveCoordinates()
+        coords.resize(2)
+        x.append(coords[0]);
+        y.append(coords[1]);
+        if (field):
+            val = oofempy.FloatArray(6)
+            field.evaluateAt(val, coords, oofempy.ValueModeType.VM_Total, tstep) 
+            z.append(val[0])
+
+    #print(nodes)
+    for i in range(1, d.giveNumberOfElements()+1):
+        e=d.giveElement(i)
+        gt=e.giveGeometryType()
+        if (gt == oofempy.Element_Geometry_Type.EGT_triangle_1):
+            elements.append((e.giveDofManager(1).giveNumber()-1, e.giveDofManager(2).giveNumber()-1, e.giveDofManager(3).giveNumber()-1))
+    triangulation = tri.Triangulation(x, y, elements)
+
+    fig, ax = plt.subplots()
+    #plt.set_title('3D OOFEM mesh')
+    ax.set_aspect('equal')
+
+    if (field):
+        levels = np.linspace(min(z), max(z), 12)
+        plt.tricontourf(triangulation, z, levels=levels, alpha=1.0)
+        plt.colorbar(orientation='horizontal')
+    else:
+        plt.triplot(triangulation, linewidth=0.2, color='black', alpha=1.0)
