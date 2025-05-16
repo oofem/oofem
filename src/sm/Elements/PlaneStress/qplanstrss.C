6865a283d1a183308a6ddaa2ecdcabc52006b549
Smilauer Vit <vit.smilauer@fsv.cvut.cz>
2025-05-16 11:38:39 +0200

    Added SpatialLocalizerInterface to quadratic plane stress element.

--- src/sm/Elements/PlaneStress/qplanstrss.C
+++ src/sm/Elements/PlaneStress/qplanstrss.C
@@ -53,7 +53,7 @@ REGISTER_Element(QPlaneStress2d);
 FEI2dQuadQuad QPlaneStress2d :: interpolation(1, 2);
 
 QPlaneStress2d :: QPlaneStress2d(int n, Domain *aDomain) :
-    PlaneStressElement(n, aDomain), ZZNodalRecoveryModelInterface(this), NodalAveragingRecoveryModelInterface()
+    PlaneStressElement(n, aDomain), ZZNodalRecoveryModelInterface(this), NodalAveragingRecoveryModelInterface(), SpatialLocalizerInterface(this)
     // Constructor.
 {
     numberOfDofMans  = 8;
@@ -67,6 +67,8 @@ QPlaneStress2d :: giveInterface(InterfaceType interface)
         return static_cast< ZZNodalRecoveryModelInterface * >(this);
     } else if ( interface == NodalAveragingRecoveryModelInterfaceType ) {
         return static_cast< NodalAveragingRecoveryModelInterface * >(this);
+    } else if ( interface == SpatialLocalizerInterfaceType ) {
+        return static_cast< SpatialLocalizerInterface * >(this);
     }
 
     return NULL;
--- src/sm/Elements/PlaneStress/qplanstrss.h
+++ src/sm/Elements/PlaneStress/qplanstrss.h
@@ -39,6 +39,7 @@ protected:
 #include "sm/Elements/structural2delement.h"
 #include "zznodalrecoverymodel.h"
 #include "nodalaveragingrecoverymodel.h"
+#include "spatiallocalizer.h"
 
 #define _IFT_QPlaneStress2d_Name "qplanestress2d"
 
@@ -49,7 +50,7 @@ class FEI2dQuadQuad;
  * This class implements an Quadratic isoparametric eight-node quadrilateral plane-
  * stress elasticity finite element. Each node has 2 degrees of freedom.
  */
-class QPlaneStress2d : public PlaneStressElement, public ZZNodalRecoveryModelInterface, public NodalAveragingRecoveryModelInterface
+class QPlaneStress2d : public PlaneStressElement, public ZZNodalRecoveryModelInterface, public NodalAveragingRecoveryModelInterface, public SpatialLocalizerInterface
 {
 protected:
     static FEI2dQuadQuad interpolation;
