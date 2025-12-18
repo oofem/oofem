4ba2edd6807c75a3c42e883112fc29897cd00d2d
Václav Šmilauer <eu@doxos.eu>
2025-12-18 22:01:05 +0100

    fix InputReader ownership crash in Python (use clone() instead of ptr())

--- src/core/engngm.C
+++ src/core/engngm.C
@@ -309,7 +309,8 @@ EngngModel :: initializeFrom(InputRecord &ir)
     IR_GIVE_OPTIONAL_FIELD(ir, profileOpt, _IFT_EngngModel_profileOpt);
 
     // get explicit nmsteps param (text), or size of the <Metasteps> sub-group (xml)
-    nMetaSteps = ir.giveReader()->giveGroupRecords(ir.ptr(),_IFT_EngngModel_nmsteps,"Metasteps",DataReader::IR_mstepRec,/*optional*/true).size();
+    /* needs to use clone() and not ptr()... unclear why; the ownership of InputRecord should be specified better */
+    nMetaSteps = ir.giveReader()->giveGroupRecords(ir.clone(),_IFT_EngngModel_nmsteps,"Metasteps",DataReader::IR_mstepRec,/*optional*/true).size();
 
     int _val = 1;
     IR_GIVE_OPTIONAL_FIELD(ir, _val, _IFT_EngngModel_nonLinFormulation);
